cfc516a4030075d4b9178e2298c592854a1bd165
Fatima Altaf <fatima.altaf@gmail.com>
2019-10-22 20:17:35 +0000

    reworked timer code

--- timer1.js
+++ timer1.js
@@ -1,6 +1,6 @@
 // ALARM CLOCK 
 const data = process.argv
-let number = data.slice(2);
+let number = data.slice(2).filter(function(number){return number > -1});
 
 const timer = function(number) {
  for (const num of number) {
